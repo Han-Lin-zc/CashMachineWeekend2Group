ce69e0b5c6e69941e116270ce51c242d7fa20583
hlin <Zxsaqw12@&$>
2020-02-23 17:20:18 -0500

    1/4 Test Classes work

---
+++ src/test/java/rocks/zipcode/atm/CashMachineTest.java
@@ -0,0 +1,47 @@
+package rocks.zipcode.atm;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import rocks.zipcode.atm.bank.Bank;
+
+import static org.junit.Assert.*;
+
+public class CashMachineTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void login() {
+    }
+
+    @Test
+    public void deposit() {
+    }
+
+    @Test
+    public void withdraw() {
+    }
+
+    @Test
+    public void exit() {
+    }
+
+    @Test
+    public void getBank() {
+    }
+
+    @Test
+    public void testToString() {
+    }
+
+    @Test
+    public void getAccountData() {
+    }
+}
--- src/test/java/rocks/zipcode/atm/bank/AccountTest.java
+++ src/test/java/rocks/zipcode/atm/bank/AccountTest.java
@@ -8,6 +8,7 @@ public class AccountTest {
 
     @Test
     public void getAccountData() {
+
     }
 
     @Test
